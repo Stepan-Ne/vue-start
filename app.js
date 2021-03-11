1e29c4150b8ead855c33596db89825c1737d2035
Stepan <nemail.necom@gmail.com>
2021-03-11 18:13:49 +0300

    interpolation of method

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ data() {
   return {
     title: 'List',
     placeholderString: 'write here',
-    inputValue: ""
+    inputValue: "",
+    notes: ['Item 1', 'Item 2'],
+    requireField: "The field is require",
+    fieldFlag: false
   }
 },
 methods: {
   inputChangeHandler(e) {
+    this.fieldFlag = false
     this.inputValue = e.target.value
+  },
+  addNote() {
+    if (!this.inputValue.trim()) {
+      this.fieldFlag = true
+      return
+    }
+    this.notes.push(this.inputValue)
+    this.inputValue = ''
+  },
+  removeNote(id) {
+    this.notes.splice(id, 1)
+  },
+  upperCase(item) {
+    return item.toUpperCase()
   }
 }
+
 }
 
 Vue.createApp(App).mount('#counter')
